e78acc84b72661891dce5b8b9c07dd39d50baadf
Sibdroid <ryabovglebrg@gmail.com>
2023-08-05 22:28:34 +0200

    Add project files.

---
+++ WordGame/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WordGame
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Word00 = new System.Windows.Forms.Button();
+			this.Word01 = new System.Windows.Forms.Button();
+			this.Word02 = new System.Windows.Forms.Button();
+			this.Word03 = new System.Windows.Forms.Button();
+			this.Word13 = new System.Windows.Forms.Button();
+			this.Word12 = new System.Windows.Forms.Button();
+			this.Word11 = new System.Windows.Forms.Button();
+			this.Word10 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// Word00
+			// 
+			this.Word00.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Word00.Location = new System.Drawing.Point(1, 361);
+			this.Word00.Name = "Word00";
+			this.Word00.Size = new System.Drawing.Size(55, 55);
+			this.Word00.TabIndex = 0;
+			this.Word00.UseVisualStyleBackColor = true;
+			// 
+			// Word01
+			// 
+			this.Word01.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Word01.Location = new System.Drawing.Point(61, 361);
+			this.Word01.Name = "Word01";
+			this.Word01.Size = new System.Drawing.Size(55, 55);
+			this.Word01.TabIndex = 1;
+			this.Word01.UseVisualStyleBackColor = true;
+			// 
+			// Word02
+			// 
+			this.Word02.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Word02.Location = new System.Drawing.Point(121, 361);
+			this.Word02.Name = "Word02";
+			this.Word02.Size = new System.Drawing.Size(55, 55);
+			this.Word02.TabIndex = 2;
+			this.Word02.UseVisualStyleBackColor = true;
+			// 
+			// Word03
+			// 
+			this.Word03.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Word03.Location = new System.Drawing.Point(181, 361);
+			this.Word03.Name = "Word03";
+			this.Word03.Size = new System.Drawing.Size(55, 55);
+			this.Word03.TabIndex = 3;
+			this.Word03.UseVisualStyleBackColor = true;
+			// 
+			// Word13
+			// 
+			this.Word13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Word13.Location = new System.Drawing.Point(181, 421);
+			this.Word13.Name = "Word13";
+			this.Word13.Size = new System.Drawing.Size(55, 55);
+			this.Word13.TabIndex = 7;
+			this.Word13.UseVisualStyleBackColor = true;
+			// 
+			// Word12
+			// 
+			this.Word12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Word12.Location = new System.Drawing.Point(121, 421);
+			this.Word12.Name = "Word12";
+			this.Word12.Size = new System.Drawing.Size(55, 55);
+			this.Word12.TabIndex = 6;
+			this.Word12.UseVisualStyleBackColor = true;
+			// 
+			// Word11
+			// 
+			this.Word11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Word11.Location = new System.Drawing.Point(61, 421);
+			this.Word11.Name = "Word11";
+			this.Word11.Size = new System.Drawing.Size(55, 55);
+			this.Word11.TabIndex = 5;
+			this.Word11.UseVisualStyleBackColor = true;
+			// 
+			// Word10
+			// 
+			this.Word10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Word10.Location = new System.Drawing.Point(1, 421);
+			this.Word10.Name = "Word10";
+			this.Word10.Size = new System.Drawing.Size(55, 55);
+			this.Word10.TabIndex = 4;
+			this.Word10.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(238, 481);
+			this.Controls.Add(this.Word13);
+			this.Controls.Add(this.Word12);
+			this.Controls.Add(this.Word11);
+			this.Controls.Add(this.Word10);
+			this.Controls.Add(this.Word03);
+			this.Controls.Add(this.Word02);
+			this.Controls.Add(this.Word01);
+			this.Controls.Add(this.Word00);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button Word00;
+		private System.Windows.Forms.Button Word01;
+		private System.Windows.Forms.Button Word02;
+		private System.Windows.Forms.Button Word03;
+		private System.Windows.Forms.Button Word13;
+		private System.Windows.Forms.Button Word12;
+		private System.Windows.Forms.Button Word11;
+		private System.Windows.Forms.Button Word10;
+	}
+}
+
---
+++ WordGame/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WordGame
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			List<string> startWords = File.ReadAllLines("words-start.txt").ToList();
+			string first = Tools.getRandomString(startWords);
+			List<string> startWordsSecond = new List<string>();
+			int index = 0;
+			foreach (string word in startWords)
+			{
+				bool fits = true;
+				foreach (char character in  word)
+				{
+					if (first.Contains(character))
+					{
+						fits = false;
+					}
+				}
+				if (fits)
+				{
+					startWordsSecond.Add(word);
+					index++;
+				}
+			}
+			string second = Tools.getRandomString(startWordsSecond);
+			foreach (Button button in this.Controls.OfType<Button>())
+			{
+				button.BackColor = Tools.getColor("#DFDFDF");
+				button.ForeColor = Tools.getColor("#000000");
+				button.FlatStyle = FlatStyle.Flat;
+				button.FlatAppearance.BorderColor = Tools.getColor("#DFDFDF");
+				button.FlatAppearance.BorderSize = 1;
+				if (button.Name.Contains("Word0"))
+				{
+					button.Text = first[button.Name[button.Name.Length - 1] - '0'].ToString().ToUpper();
+				}
+				else if (button.Name.Contains("Word1"))
+				{
+					button.Text = second[button.Name[button.Name.Length - 1] - '0'].ToString().ToUpper();
+				}
+			}
+		}
+	}
+	public class Tools
+	{
+		static Random random = new Random();
+		public static System.Drawing.Color getColor (string code)
+		{
+			return System.Drawing.ColorTranslator.FromHtml (code);
+		}
+		public static string getRandomString (List <string> values)
+		{
+			int index = random.Next(0, values.Count);
+			return values[index];
+		}
+	}
+}
